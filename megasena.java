ea2b87e175084c96706b540f7204befbc999e38e
Caio Dantas <105518083+DntsC@users.noreply.github.com>
2023-04-23 17:23:15 -0300

    Update megasena.java

--- megasena.java
+++ megasena.java
@@ -5,18 +5,10 @@ public class megasena {
     public static void main(String[] args) {
         Random generate = new Random();
 
-//      int i = 0;
-//      while(i < 6) {                          //Enquanto i for menor que 6, um número aleatório até 60 será gerado
-//          int number = generate.nextInt(60);
-//          System.out.println(number);
-//          i++;
-//      }
-
         for(int i = 0; i < 6; i++) {
             int number = generate.nextInt(60);
             System.out.println(number);
         }
-     // O comando usando o for tem a mesma finalidade do while
 
     }
 }
